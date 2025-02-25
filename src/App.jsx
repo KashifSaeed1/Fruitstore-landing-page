54d5baef4a370e321ac5deeb83bbb78f9b6f8064
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 11:57:22 +0500

    Add responsive and well-structured Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Menus from "./components/Menus/Menus";
 import Banner from "./components/Banner/Banner";
 import Banner2 from "./components/Banner/Banner2";
 import Banner3 from "./components/Banner/Banner3";
+import Footer from "./components/Footer/Footer";
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Banner />
         <Banner2 />
         <Banner3 />
+        <Footer/>
       </main>
     </>
   );

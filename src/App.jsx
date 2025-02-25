7adf97ce3845bf067e9576db77bad8df0a55dd45
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 11:52:34 +0500

    Add vibrant Fruit Banner sections to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Menus from "./components/Menus/Menus";
+import Banner from "./components/Banner/Banner";
+import Banner2 from "./components/Banner/Banner2";
+import Banner3 from "./components/Banner/Banner3";
+
 
 const App = () => {
   return (
     <>
       <main className=" overflow-x-hidden">
         <NavBar />
-        <Hero/>
-        <Menus/>
+        <Hero />
+        <Menus />
+        <Banner />
+        <Banner2 />
+        <Banner3 />
       </main>
     </>
   );

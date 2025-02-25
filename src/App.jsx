6403594fdb5ac0dbaa11fdba8851938388231e96
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 11:35:56 +0500

    Add engaging Hero section to the FruitFood store homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
 const App = () => {
   return (
     <>
       <main className=" overflow-x-hidden">
         <NavBar />
+        <Hero/>
       </main>
     </>
   );
 };
-
 export default App;

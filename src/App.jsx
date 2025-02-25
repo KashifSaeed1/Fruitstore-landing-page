0a1f3429d0b2a388991a67bf2ead1fbd9af7a89b
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 11:43:23 +0500

    Add interactive and well-structured Menu section to the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Menus from "./components/Menus/Menus";
+
 const App = () => {
   return (
     <>
       <main className=" overflow-x-hidden">
         <NavBar />
         <Hero/>
+        <Menus/>
       </main>
     </>
   );

a4335189c50d73c8153a15085cd9b3c89b54f171
Kashif Saeed <kashifsaeedman@gmail.com>
2025-02-25 11:30:16 +0500

    Add responsive and stylish navbar to FruitFood store

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar/NavBar";
+const App = () => {
+  return (
+    <>
+      <main className=" overflow-x-hidden">
+        <NavBar />
+      </main>
+    </>
+  );
+};
+
+export default App;
